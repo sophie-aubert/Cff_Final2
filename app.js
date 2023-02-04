e880b4aabad0bdcb1a77ef8293d7536f5e1dbe47
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-02-04 14:02:41 +0100

    Gestion d'erreur

--- app.js
+++ app.js
@@ -2,15 +2,37 @@
 const afficheTitre = document.querySelector("h1")
 const afficheBoard = document.querySelector("#board")
 
+
 const recupDonnees = ((ville) =>{
-    fetch (`https://transport.opendata.ch/v1/stationboard?station=${ville}&limit=10`)
+        fetch (`https://transport.opendata.ch/v1/stationboard?station=${ville}&limit=10`)
     .then((resultat) => { 
+
+        // ON GÈRE L'ERREUR DE VILLE INEXISTANTE
+        if (resultat.status === 404){
+            throw new Error("Oupss.")} 
+        // **************************************
         return resultat.json()})
     .then((data) => {
+
+        // ON GÈRE L'ERREUR 404 
+        if (data.stationboard.length === 0){
+            throw new Error(
+                afficheTitre.innerHTML = "Oupss...ce n'est pas une ville ! "
+            )}
+        // *********************
+
         data.stationboard.forEach((element) => affichage(element));
-        afficheTitre.innerHTML = data.station.name
+        afficheTitre.innerHTML = ville
     })
-})
+    
+    // GERE TOUTES LES ERREURS DANS LA CONSOLE
+    // On le met toujours à la fin dès qu'on traite une erreur. 
+    .catch((err) => {
+        console.log(err.message);
+      });
+    // **************************
+
+      })
 
 recupDonnees("lausanne");
 
